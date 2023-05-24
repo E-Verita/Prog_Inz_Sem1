5cf622ab5a8c0c84ed057d4aeca4fdc081e886c1
Everita Vecbērza <everita.vecberza@gmail.com>
2023-05-24 22:12:19 +0300

    Add: ICRUDProductService, IFilteringProductService

---
+++ src/main/java/lv/venta/services/IFilteringProductService.java
@@ -0,0 +1,9 @@
+package lv.venta.services;
+
+import java.util.ArrayList;
+
+import lv.venta.models.Product;
+
+public interface IFilteringProductService {
+	ArrayList <Product> filterByPriceLess(float price);
+}
